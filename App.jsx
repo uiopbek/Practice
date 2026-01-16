ebb3efa7afa55401f0b4489e4eeaedd84b269a92
uiopbek <uiopyura@gmail.com>
2026-01-16 13:45:09 +0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,151 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="navbar">
+        <div className='navbar1'>
+          <img className='lf' src="/lf.svg" alt="" />
+          <p className='led'>LeadFlow</p>
+          <p className='home'>Home</p>
+          <img className='qiyshaygan1' src="/qiyshaygan1.svg" alt="" />
+          <p className='dashboard'>Dashboard</p>
+        </div>
+        <div className='navbar2'>
+          <input className='inp' type="text" placeholder='Search leads, contacts, deals...' />
+        </div>
+        <div className='navbar3'>
+          <button className='btn'><p className='btnplus'> +</p> <p className='btnled'>Add Lead</p> </button>
+          <img className='togri' src="/togri.svg" alt="" />
+          <img className='qongiroq' src="/qongiroq.svg" alt="" />
+          <img className='soroq' src="/soroq.svg" alt="" />
+          <img className='togri2' src="/togri2.svg" alt="" />
+          <div className='oxirinav'>
+            <img className='jd' src="/jd.svg" alt="" />
+            <div className='jon'>
+              <p className='jon1'>JohnDoe</p>
+              <p className='jon2'>Admin</p>
+            </div>
+            <img className='oxirgi' src="oxirgi.svg" alt="" />
+          </div>
+        </div>
+      </div>
+
+      <div className='logopasi'>
+        <div className='logopasi1'>
+          <img className='logo1' src="/logo1.svg" alt="" />
+          <img className='logo2' src="/logo2.svg" alt="" />
+          <img className='logo3' src="/logo3.svg" alt="" />
+          <img className='logo4' src="/logo4.svg" alt="" />
+          <img className='logo5' src="/logo5.svg" alt="" />
+          <img className='logo6' src="/logo6.svg" alt="" />
+          <img className='logo7' src="/logo7.svg" alt="" />
+          <img className='logo8' src="/logo8.svg" alt="" />
+          <img className='logo9' src="/logo9.svg" alt="" />
+          <img className='logo10' src="/logo10.svg" alt="" />
+          <img className='logo11' src="/logo11.svg" alt="" />
+          <img className='logo12' src="/logo12.svg" alt="" />
+        </div>
+
+        <div className='logopasi2'>
+          <img className='logo13' src="/logo13.svg" alt="" />
+          <img className='logo14' src="/logo14.svg" alt="" />
+        </div>
+      </div>
+
+      <div className='dashboard11'>
+
+
+        <div className='dw'>
+          <p className='dashboard1'>Dashboard Overview</p>
+          <p className='dashboard2'>Welcome back, here's what's happening today.</p>
+        </div>
+
+
+        <div className='logos'>
+          <img className='dlogo1' src="/dlogo1.svg" alt="" />
+          <p className='filter'>Filter</p>
+
+
+          <img className='dlogo2' src="/dlogo2.svg" alt="" />
+          <p className='export'>Export</p>
+        </div>
+
+      </div>
+
+      <div className='fghh'>
+
+        <div>
+          <div className='err'>
+            <div className='tl'>
+              <div>
+                <p className='t'>Total Leads</p>
+              </div>
+              <div className='vfr'>
+                <p className='v'>+12.5%</p>
+                <img className='fr' src="/frame15.svg" alt="" />
+              </div>
+            </div>
+            <div>
+              <p className='p'>1.248</p>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div className='errr'>
+            <div className='tll'>
+              <div>
+                <p className='tt'>Conversion Rate</p>
+              </div>
+              <div className='vfrr'>
+                <p className='vv'>-0.4%</p>
+                <img className='frr' src="/frame16.svg" alt="" />
+              </div>
+            </div>
+            <div>
+              <p className='pp'>3.2%</p>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div className='errrr'>
+            <div className='tlll'>
+              <div>
+                <p className='ttt'>Active Deals</p>
+              </div>
+              <div className='vfrrr'>
+                <p className='vvvv'>+8.2%</p>
+                <img className='frrr' src="/frame15.svg" alt="" />
+              </div>
+            </div>
+            <div>
+              <p className='ppp'>45</p>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div className='errrrr'>
+            <div className='tllll'>
+              <div>
+                <p className='tttt'>Revenue</p>
+              </div>
+              <div className='vfrrrr'>
+                <p className='vvvvv'>+15.3%</p>
+                <img className='frrrr' src="/frame15.svg" alt="" />
+              </div>
+            </div>
+            <div>
+              <p className='pppp'>$24.5k</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
